c4450e482c116affaa9bd1cba39b10e6494a8f00
mark-marshall <mark.marshallgp@gmail.com>
2019-02-22 09:27:30 +0000

    [STRETCH] Added infinite scroll Carousel Component

--- lambdatimes/src/components/Carousel/Carousel.js
+++ lambdatimes/src/components/Carousel/Carousel.js
@@ -5,28 +5,44 @@ export default class Carousel extends Component {
   constructor(props){
     super(props);
     this.state = {
-
+      images: [],
+      arrayRefOfCurrent: 0,
     }
   }
   componentDidMount(){
-
+    this.setState({
+      images: carouselData,
+    })
   }
 
   leftClick = () => {
-
+    if (this.state.arrayRefOfCurrent === 0){
+      this.setState({
+        arrayRefOfCurrent: 3,
+      })
+    } else {
+      this.setState (prevState => ({
+        arrayRefOfCurrent: prevState.arrayRefOfCurrent -1,
+      }))
+    }
   }
 
   rightClick = () => {
-
-  }
-
-  selectedImage = () => {
-    return <img src={} style={{display: 'block'}} />
+    if (this.state.arrayRefOfCurrent === 3){
+      this.setState({
+        arrayRefOfCurrent: 0,
+      })
+    } else {
+      this.setState (prevState => ({
+        arrayRefOfCurrent: prevState.arrayRefOfCurrent +1,
+      }))
+    }
   }
   
   render(){
     return (
       <div className="carousel">
+        <img src={this.state.images[this.state.arrayRefOfCurrent]} style={{display: 'block'}} />
         <div className="left-button" onClick={this.leftClick}>{"<"}</div>
         <div className="right-button" onClick={this.rightClick}>{">"}</div>
       </div>
--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import Tabs from './Tabs';
 import Cards from './Cards';
+import Carousel from '../Carousel/Carousel';
 
 // Importing our tab and card data. No need to change anything here.
 import { tabData, cardData } from '../../data';
@@ -63,6 +64,7 @@ export default class Content extends Component {
           selectedTab={this.selectedTab}
           selectTabHandler={this.changeSelected}
         />
+        <Carousel />
         <Cards cards={this.filterCards()} />
       </div>
     );
