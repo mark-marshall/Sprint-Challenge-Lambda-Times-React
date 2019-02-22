7e97d002b35c1cb2aed49706bd064997cc75d198
mark-marshall <mark.marshallgp@gmail.com>
2019-02-22 09:08:22 +0000

    [MVP] Added styled-components to TopBar Component

--- lambdatimes/src/components/TopBar.js
+++ lambdatimes/src/components/TopBar.js
@@ -1,24 +1,168 @@
 import React from 'react';
+import styled from 'styled-components';
 
-// Refactor this component to use styled components and not classNames. 
+// Styled-Components
+const TopBarStyles = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: none;
+  flex-direction: row;
+  position: fixed;
+  height: 44px;
+  background-color: #333;
+  display: flex;
+  justify-content: none;
+  align-items: center;
+  flex-direction: row;
+  flex: 1;
+  font-size: 11px;
+  cursor: pointer;
+  margin-right: 25%;
+  font-weight: bold;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  flex: 3;
+  font-size: 9px;
+  cursor: pointer;
+  margin-right: 5%;
+  margin-right: 0;
+  text-decoration: underline;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  flex-direction: row;
+  flex: 1;
+  font-size: 11px;
+  font-weight: bold;
+  cursor: pointer;
+
+  @media (min-width: 1280px) {
+    width: 1280px;
+  }
+`;
+
+const TopBarContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: none;
+  align-items: none;
+  flex-direction: row;
+  color: #fff;
+  letter-spacing: 1px;
+  padding: 0 10px;
+  display: flex;
+  justify-content: none;
+  align-items: center;
+  flex-direction: row;
+  flex: 1;
+  font-size: 11px;
+  cursor: pointer;
+  margin-right: 25%;
+  font-weight: bold;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  flex: 3;
+  font-size: 9px;
+  cursor: pointer;
+  margin-right: 5%;
+  margin-right: 0;
+  text-decoration: underline;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  flex-direction: row;
+  flex: 1;
+  font-size: 11px;
+  font-weight: bold;
+  cursor: pointer;
+
+  @media (min-width: 1280px) {
+    width: 1280px;
+  }
+`;
+
+const TopBarContainerLeft = styled.div`
+  display: flex;
+  justify-content: none;
+  align-items: center;
+  flex-direction: row;
+  flex: 1;
+  font-size: 11px;
+  cursor: pointer;
+  margin-right: 25%;
+  font-weight: bold;
+`;
+
+const TopBarSpan = styled.span`
+  cursor: pointer;
+  margin-right: 25%;
+  font-weight: bold;
+  cursor: pointer;
+  margin-right: 5%;
+  cursor: pointer;
+
+  &:hover {
+    text-decoration: underline;
+  }
+
+  &:last-child {
+    margin-right: 0;
+  }
+`;
+
+const TopBarContainerCenter = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  flex: 3;
+  font-size: 9px;
+  cursor: pointer;
+  margin-right: 5%;
+  margin-right: 0;
+  text-decoration: underline;
+`;
+
+const TopBarContainerRight = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  flex-direction: row;
+  flex: 1;
+  font-size: 11px;
+  font-weight: bold;
+  cursor: pointer;
+`;
+
+// Refactor this component to use styled components and not classNames.
 // You can find the corresponding CSS in the CSS/index.css file
 
 const TopBar = () => {
   return (
-    <div className="top-bar">
-      <div className="container">
-        <div className="container-left">
-          <span>TOPICS</span><span>SEARCH</span>
-        </div>
-        <div className="container-center">
-          <span>GENERAL</span><span>BROWNBAG</span><span>RANDOM</span><span>MUSIC</span><span>ANNOUNCEMENTS</span>
-        </div>
-        <div className="container-right">
-          <span>LOG IN</span>
-        </div>
-      </div>
-    </div>
-  )
-}
+    <TopBarStyles>
+      <TopBarContainer>
+        <TopBarContainerLeft>
+          <TopBarSpan>TOPICS</TopBarSpan>
+          <TopBarSpan>SEARCH</TopBarSpan>
+        </TopBarContainerLeft>
+        <TopBarContainerCenter>
+          <TopBarSpan>GENERAL</TopBarSpan>
+          <TopBarSpan>BROWNBAG</TopBarSpan>
+          <TopBarSpan>RANDOM</TopBarSpan>
+          <TopBarSpan>MUSIC</TopBarSpan>
+          <TopBarSpan>ANNOUNCEMENTS</TopBarSpan>
+        </TopBarContainerCenter>
+        <TopBarContainerRight>
+          <TopBarSpan>LOG IN</TopBarSpan>
+        </TopBarContainerRight>
+      </TopBarContainer>
+    </TopBarStyles>
+  );
+};
 
 export default TopBar;
